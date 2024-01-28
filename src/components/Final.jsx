650144abe94b5c6aec4f993df80a135ef2462ddf
Hirukamunasinghe <munasinghehiruka@gmail.com>
2024-01-28 11:41:39 +0530

    commit

--- src/components/Final.jsx
+++ src/components/Final.jsx
@@ -7,7 +7,7 @@ const Final = () =>{
     return(
         <div className="final-pg-div">
             <h1>VISUALIZATION RESULTS</h1>
-            <h3 className="des">This visualized AI Avatar displays<br/> how your body will be in a couple<br/> of months if you have completed<br/> this exercise accurately</h3>
+            <h3 className="des">This visualized AI Avatar displays how your body will<br/> be in a couple of months<br/> if you have completed<br/> this exercise accurately</h3>
             <img src={body} className="bodyimage" alt=""/>
             <Link to="/">
             <button className="finalbtn">HOME</button>
